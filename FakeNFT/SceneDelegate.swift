a6a559d66f12d4bcfa2032f429c34f4536d06041
vadimrn <120738327+vadimrn@users.noreply.github.com>
2023-11-09 22:28:07 +0300

    метод, который вызывается, когда новая сцена подключается к приложению

--- FakeNFT/SceneDelegate.swift
+++ FakeNFT/SceneDelegate.swift
@@ -3,20 +3,11 @@ import UIKit
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    
-
-    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
-        
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.backgroundColor = .systemBackground
-            
-            let tabBarController = TabBarController()
-            
-            window.rootViewController = tabBarController
-            
-            self.window = window
-            window.makeKeyAndVisible()
-        }
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+        window.rootViewController = MainTabBarController()
+        self.window = window
+        window.makeKeyAndVisible()
     }
 }

f13dbca7f5425b8eab3a2f3643681955c632510a
vadimrn <120738327+vadimrn@users.noreply.github.com>
2023-11-09 22:26:18 +0300

    AppDelegate является точка входа в приложение

--- FakeNFT/AppDelegate.swift
+++ FakeNFT/AppDelegate.swift
@@ -11,11 +11,11 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
-    func application(
-        _: UIApplication,
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options _: UIScene.ConnectionOptions
-    ) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let configuration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        configuration.storyboard = nil
+        configuration.sceneClass = UIWindowScene.self
+        configuration.delegateClass = SceneDelegate.self
+        return configuration
     }
 }

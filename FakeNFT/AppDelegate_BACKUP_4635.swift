6e18cdc363ec68f83b81110447e0dda4ac04cf80
vadimrn <120738327+vadimrn@users.noreply.github.com>
2023-12-24 20:17:35 +0500

    сборка приложения

--- FakeNFT/AppDelegate_BACKUP_4635.swift
+++ FakeNFT/AppDelegate_BACKUP_4635.swift
@@ -9,16 +9,14 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-<<<<<<< HEAD
         UINavigationBar.appearance().tintColor = UIColor.black
         window = UIWindow()
         window?.rootViewController = TabBarController()
         window?.makeKeyAndVisible()
-=======
         ProgressHUD.animationType = .systemActivityIndicator
         ProgressHUD.colorHUD = UIColor(resource: .blackDayNight)
         ProgressHUD.colorAnimation = UIColor(resource: .lightDayNight)
->>>>>>> 05985f48a6956abe56e5a88d9e95632a2b474f24
+
         return true
     }
 }
